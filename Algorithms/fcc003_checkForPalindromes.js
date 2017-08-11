5771b80f3753beae9c4c283773e492d504198a85
Bob King <bob@someotherdude.com>
2017-08-11 08:33:07 -0400

    refactored palindromes from recovered file. _FINAL_

--- Algorithms/fcc003_checkForPalindromes.js
+++ Algorithms/fcc003_checkForPalindromes.js
@@ -7,10 +7,23 @@
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
 
 function palindrome(str) {
-  // Good luck!
-  return true;
+    // Good luck!
+    var revStr = ""; // reversed str
+    var pool = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+    // 1. convert to lowercase
+    // 2. get rid of non-alphanumeric characters
+    str = str.toLowerCase().split("").map(function (ch) {
+        if(pool.includes(ch)) {
+            // 3. create reversed string
+            revStr = ch + revStr;
+            return ch;
+        }
+    }).join("");
+
+    // 4. Compare and return result
+    return (str==revStr);
+
 }
-
-
 
 palindrome("eye");
