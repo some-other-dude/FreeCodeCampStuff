7245c422d454b5775095083aacd0fcf8c314af1a
Bob King <bking39116@gmail.com>
2017-08-10 23:13:41 -0400

    Added skeleton files for the first three exercises. Will recode tomorrow

---
+++ fcc001_reverseAString.js
@@ -0,0 +1,10 @@
+// fcc001_reverseAString.js
+// Reverse the provided string.
+// You may need to turn the string into an array before you can reverse it.
+// Your result must be a string.
+
+function reverseString(str) {
+  return str;
+}
+
+reverseString("hello");
