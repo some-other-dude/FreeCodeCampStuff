3fc96b5883a8445bdaa9c689bf3000b1d58e1980
Bob King <bob@someotherdude.com>
2017-08-11 20:59:11 -0400

    f012_mutations.js refactored _FINAL_

--- Algorithms/f012_mutations.js
+++ Algorithms/f012_mutations.js
@@ -1,12 +1,12 @@
 // f012_mutations.js
 //
-// Return true if the string in the first element of the array contains all of
-// the letters of the string in the second element of the array.
+// Return true if the arr[1]ing in the first element of the array contains all of
+// the letters of the arr[1]ing in the second element of the array.
 //
 // For example, ["hello", "Hello"], should return true because all of the letters
-// in the second string are present in the first, ignoring case.
+// in the second arr[1]ing are present in the first, ignoring case.
 //
-// The arguments ["hello", "hey"] should return false because the string "hello"
+// The arguments ["hello", "hey"] should return false because the arr[1]ing "hello"
 // does not contain a "y".
 //
 // Lastly, ["Alien", "line"], should return true because all of the letters in
@@ -15,20 +15,14 @@
 // true if all letters of arr[1] are in arr[0]
 
 function mutation(arr) {
-    var str1 = arr[0];
-    var str2 = arr[1];
-
-    function compareStrings(str, pool) {
         var same = true;
-        pool = pool.toLowerCase();
-        str.toLowerCase().split("").map(function (ch) {
-            if (pool.includes(ch) == false) {
+        arr[0] = arr[0].toLowerCase();
+        arr[1].toLowerCase().split("").map(function (ch) {
+            if (arr[0].includes(ch) == false) {
                 same = false;
             }
         });
         return same;
-    }
-    return compareStrings(str2, str1);
 }
 
 console.log(mutation(["Hello", "hey"]));
